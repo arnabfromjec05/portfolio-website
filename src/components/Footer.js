438d9a0dc0c163338a4cbaec458a64c5b9c9c34f
Arnab Jana <arnabfromjec05@gmail.com>
2022-10-30 14:36:51 +0530

    footer ready

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,53 @@
+import { Grid, Paper, Toolbar, Typography } from "@mui/material";
+
 const Footer = () => {
     return (
-        <div>
-            Footer comes here
-        </div>
+        <Paper sx={{
+            marginTop: 'calc(10% + 60px)',
+            width: '100%',
+            position: 'fixed',
+            bottom: 0,
+            py: '1%'
+        }} component="footer" square variant="outlined">
+            <Toolbar>
+                <Grid container sx={{ fontSize: '12px' }} rowSpacing={1}>
+                    <Grid item sm={6} xs={12}>
+                        <Typography>Copyright © 2022 Arnab Jana - All Rights Reserved</Typography>
+                    </Grid>
+                    <Grid item container sm={6} xs={12} rowSpacing={2} direction="column">
+                        <Grid item>
+                            <Typography>Follow me on:</Typography>
+                        </Grid>
+                        <Grid item container columnSpacing={2}>
+                            <Grid item>
+                                <a href="https://www.linkedin.com/in/arnab-jana/"
+                                    aria-label="linkedin"
+                                    target="_blank"
+                                    rel="noreferrer">
+                                    <img src="linkedin.png" alt="linkedin" className="logo" />
+                                </a>
+                            </Grid>
+                            <Grid item>
+                                <a href="https://www.instagram.com/arnabfromjec05/"
+                                    aria-label="instagram"
+                                    target="_blank"
+                                    rel="noreferrer">
+                                    <img src="instagram.jpeg" alt="instagram" className="logo" />
+                                </a>
+                            </Grid>
+                            <Grid item>
+                                <a href="https://twitter.com/arnabjana97/"
+                                    aria-label="twitter"
+                                    target="_blank"
+                                    rel="noreferrer">
+                                    <img src="twitter.png" alt="twitter" className="logo" />
+                                </a>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </Paper>
     )
 }
 
