a1997f0a60e1e25ac8847feb1c9853b689e534d7
Arnab Jana <arnabfromjec05@gmail.com>
2022-10-31 18:57:51 +0530

    project section added in about.js

--- src/data/about.js
+++ src/data/about.js
@@ -1,12 +1,16 @@
 
 export const INTRO_HEADER = "Hello, I am Arnab Jana";
+
 export const INTRO_DESCRIPTION = "I am a Software engineer having 2+ years of professional experience.\
     Graduated from RV College of Engineering Bengaluru in 2020 batch having majors in Computer Science & Engineering.\
     Currently i am working in Microsoft R&D, Bengaluru in their Azure team as an SDE.\
     My skillsets include programming languages and tools like C++, Java, Python, Javascript, Typescript, MySQL and MongoDB.\
     Apart from this i have experience working with Azure cloud services.";
 
+export const RESUME_LINK = "https://drive.google.com/file/d/1-e_mRDDXfcE63IrlcvF8hvzYk0xuBZfL/view?usp=share_link";
+
 export const EXPERIENCE_HEADER = "Experience";
+
 export const EXPERIENCE_LIST = [
     {
         role: "Software Engineer",
@@ -25,7 +29,34 @@ export const EXPERIENCE_LIST = [
     }
 ];
 
-export const RESUME_LINK = "https://drive.google.com/file/d/1-e_mRDDXfcE63IrlcvF8hvzYk0xuBZfL/view?usp=share_link";
+export const PROJECTS_HEADER = "Project";
 
+export const PROJECTS_LIST = [
+    {
+        title: "Impact analysis of Earthquake using Machine Learning",
+        image: "ieee.png",
+        link: "https://ieeexplore.ieee.org/document/9031026",
+        description: "The paper describes an approach to quantify the impacts of an earthquake striking an area \
+            based upon various geological, social and meteorological properties of the area using Machine Learning algorithms.\
+            Finally a conclusive intelligence is found out which can be used to draw inferences and hence mitigates future impacts."
+    },
+    {
+        title: "Parking lot management system",
+        image: "plms.png",
+        link: "https://github.com/arnabfromjec05/parking-lot-management-system",
+        description: "Application that emulates the working of a parking lot by automating the entry and exit of vehicles. Various features supported by the system are:\
+            1. Entry and exit of vehicles. Allowing entry of vehicle only when there is a free parking slot.\
+            2. Timed charges based on the stay duration.\
+            3. Virtual ticket generation for the allowed vehicles.\
+            4. Interface for the current status of slots.\
+            5. Necessary views allowed for the admin only."
+    },
+    {
+        title: "Portfolio website",
+        image: "mypic2.jpg",
+        link: "#",
+        description: "My very own portfolio website build using React + Material UI and soon going to be hosted."
+    }
+]
 
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,17 @@
-import { Button, Paper, Step, StepContent, StepLabel, Stepper, Typography } from "@mui/material";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Grid,
+    Paper,
+    Step,
+    StepContent,
+    StepLabel,
+    Stepper,
+    Typography,
+} from "@mui/material";
 import * as Data from "../data/about.js";
 
 const About = () => {
@@ -6,45 +19,43 @@ const About = () => {
         <>
             <IntroSection />
             <ExperienceSection />
+            <ProjectsSection />
         </>
-    )
-}
+    );
+};
 
 const IntroSection = () => {
     return (
         <Paper elevation={4} sx={{ margin: "2%", padding: "2%" }}>
-            <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+            <Typography variant="h6" sx={{ fontWeight: "bold", marginBottom: "1%" }}>
                 {Data.INTRO_HEADER}
             </Typography>
-            <Typography>
-                {Data.INTRO_DESCRIPTION}
-            </Typography>
+            <Typography>{Data.INTRO_DESCRIPTION}</Typography>
             <Button
                 variant="outlined"
                 color="secondary"
                 target="_blank"
                 rel="noreferrer"
                 href={Data.RESUME_LINK}
-                sx={{ marginTop: "1%" }}>
+                sx={{ marginTop: "1%" }}
+            >
                 Resume
             </Button>
-        </Paper>
-    )
-}
+        </Paper >
+    );
+};
 
 const ExperienceSection = () => {
     return (
         <>
             <Paper elevation={4} sx={{ margin: "2%", padding: "2%" }}>
-                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                <Typography variant="h6" sx={{ fontWeight: "bold", marginBottom: "1%" }}>
                     {Data.EXPERIENCE_HEADER}
                 </Typography>
                 <Stepper orientation="vertical">
-                    {Data.EXPERIENCE_LIST.map(exp => (
+                    {Data.EXPERIENCE_LIST.map((exp) => (
                         <Step key={exp.company + exp.role} active={true}>
-                            <StepLabel>
-                                {exp.role}
-                            </StepLabel>
+                            <StepLabel>{exp.role}</StepLabel>
                             <StepContent>
                                 <Typography>{exp.company}</Typography>
                                 <Typography>{exp.tenure}</Typography>
@@ -54,7 +65,51 @@ const ExperienceSection = () => {
                 </Stepper>
             </Paper>
         </>
-    )
-}
+    );
+};
+
+const ProjectsSection = () => {
+    return (
+        <>
+            <Paper elevation={4} sx={{ margin: "2%", padding: "2%" }}>
+                <Typography variant="h6" sx={{ fontWeight: "bold", marginBottom: "1%" }}>
+                    {Data.PROJECTS_HEADER}
+                </Typography>
+                <Grid container>
+                    {Data.PROJECTS_LIST.map((project) => (
+                        <Grid item xs={12} sm={4} key={project.title}>
+                            <Card sx={{ maxWidth: 320 }}>
+                                <CardMedia
+                                    component="img"
+                                    alt={project.title}
+                                    height="256"
+                                    image={project.image}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {project.title}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {project.description}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button
+                                        size="medium"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        href={project.link}
+                                    >
+                                        Show Project
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Paper>
+        </>
+    );
+};
 
 export default About;
