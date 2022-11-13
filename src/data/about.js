5f9bac03ae266cdf517f72b68879dc0cf983ee08
Arnab Jana <arnabfromjec05@gmail.com>
2022-11-13 17:23:02 +0530

    fix multiline string warning

--- src/data/about.js
+++ src/data/about.js
@@ -1,12 +1,12 @@
 export const INTRO_HEADER = "Hello, I am Arnab Jana";
 
 export const INTRO_DESCRIPTION =
-  "I am a Software engineer having 2+ years of professional experience.\
-    Graduated from RV College of Engineering Bengaluru in 2020 batch having majors in Computer Science & Engineering.\
-    Currently i am working in Microsoft R&D, Bengaluru in the Azure team as an SDE.\
-    My skillsets include programming languages: C++, Java, Python, Javascript, Typescript and Front-end libraries like React and Knockout JS.\
-    For backend I have experience working with Node.js and Java (Spring MVC Framework). In Databases I am acquainted to MySQL Relational Database and MongoDB NoSQL database.\
-    Apart from this i have experience working with Azure cloud services and building applications integrating them.";
+  "I am a Software engineer having 2+ years of professional experience. " +
+  "Graduated from RV College of Engineering Bengaluru in 2020 batch having majors in Computer Science & Engineering. " +
+  "Currently i am working in Microsoft R&D, Bengaluru in the Azure team as an SDE. " +
+  "My skillsets include programming languages: C++, Java, Python, Javascript, Typescript and Front-end libraries like React and Knockout JS. " +
+  "For backend I have experience working with Node.js and Java (Spring MVC Framework). " +
+  "In Databases I am acquainted to MySQL Relational Database and MongoDB NoSQL database. Apart from this i have experience working with Azure cloud services and building applications integrating them.";
 
 export const RESUME_LINK =
   "https://drive.google.com/file/d/1-e_mRDDXfcE63IrlcvF8hvzYk0xuBZfL/view?usp=share_link";
@@ -39,21 +39,21 @@ export const PROJECTS_LIST = [
     image: "ieee.png",
     link: "https://ieeexplore.ieee.org/document/9031026",
     description:
-      "The paper describes an approach to quantify the impacts of an earthquake striking an area \
-            based upon various geological, social and meteorological properties of the area using Machine Learning algorithms.\
-            Finally a conclusive intelligence is found out which can be used to draw inferences and hence mitigates future impacts.",
+      "The paper describes an approach to quantify the impacts of an earthquake striking an area " +
+      "based upon various geological, social and meteorological properties of the area using Machine Learning algorithms. " +
+      "Finally a conclusive intelligence is found out which can be used to draw inferences and hence mitigates future impacts.",
   },
   {
     title: "Parking lot management system",
     image: "plms.png",
     link: "https://github.com/arnabfromjec05/parking-lot-management-system",
     description:
-      "Full stack application that simulates the working of a Parking lot management, by automating the entry and exit of vehicles. Various features supported by the system are:\
-            1. Entry and exit of vehicles. Allowing entry of vehicle only when there is a free parking slot.\
-            2. Timed charges based on the stay duration.\
-            3. Virtual ticket generation for the allowed vehicles.\
-            4. Interface for the current status of slots.\
-            5. Necessary views allowed for the admin only.",
+      "Full stack application that simulates the working of a Parking lot management, by automating the entry and exit of vehicles. Various features supported by the system are:" +
+      "1. Entry and exit of vehicles. Allowing entry of vehicle only when there is a free parking slot." +
+      "2. Timed charges based on the stay duration." +
+      "3. Virtual ticket generation for the allowed vehicles." +
+      "4. Interface for the current status of slots." +
+      "5. Necessary views allowed for the admin only.",
   },
   {
     title: "Portfolio website",
